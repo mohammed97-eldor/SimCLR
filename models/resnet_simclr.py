817964464e3bef306b0ece4a71f9498b607a9959
meldor97 <mohammed.el_dor@aitemsolutions.com>
2025-02-05 13:38:43 +0100

    loaded pretrained resnet

--- models/resnet_simclr.py
+++ models/resnet_simclr.py
@@ -10,11 +10,11 @@ class ResNetSimCLR(nn.Module):
         self.embedding_dim = embedding_dim
 
         if size == 18:
-            self.model = models.resnet18(weight=None)
+            self.model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1) 
         elif size == 50:
-            self.model = models.resnet50(weights=None)
+            self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
         elif size == 101:
-            self.model = models.resnet101(weight=None)
+            self.model = models.resnet101(weights=models.ResNet101_Weights.IMAGENET1K_V1)
         else:
             print("The passed model size is nopt available, therefore irt will be set to the defaults value and ResNet50 will be used!")
             self.model = models.resnet50(pretrained=False)
